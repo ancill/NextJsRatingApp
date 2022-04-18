fa55a5d253823fefee8b7e78d4b1f08368c66bdf
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2022-04-18 18:24:34 +0300

    Fixed svg

---
+++ components/Paragraph/Paragraph.tsx
@@ -0,0 +1,15 @@
+import { ParagraphProps } from './Paragraph.props';
+import styles from './Paragraph.module.css';
+import cn from 'classnames';
+export const Paragraph = ({ children, size, className, ...props }: ParagraphProps): JSX.Element => {
+	return (
+		<p
+			className={cn(styles.p, className, {
+				[styles.s]: size === 's',
+				[styles.m]: size === 'm',
+				[styles.l]: size === 'l',
+			})}
+			{...props}
+		>{children}</p>
+	);
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import { Button, Htag } from '../components';
+import { Paragraph } from '../components/Paragraph/Paragraph';
 
 export default function Home(): JSX.Element {
 
   return (
     <div>
       <Htag tag='h2'>Hello</Htag>
-      <Button appearance='primary' arrow='down'>Helo</Button>
-      <Button appearance='ghost'>ho</Button>
+      <Button appearance='primary' arrow='right'>Helo</Button>
+      <Button appearance='ghost' arrow='down'>ho</Button>
+      <Paragraph size='l'>this is paragraph</Paragraph>
     </div>
   );
 }
