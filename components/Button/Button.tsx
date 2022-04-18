6b5d70655759138e502f4b8a4ccbd253cd7e69b1
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2022-04-18 14:00:14 +0300

    Button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,13 +1,13 @@
 import { ButtonProps } from './Button.props';
 import styles from './Button.module.css';
 import cn from 'classnames';
-export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
-
-	return <button className={cn(styles.button, {
+export const Button = ({ appearance, children, className, ...props }: ButtonProps): JSX.Element => {
+	return <button className={cn(styles.button, className, {
 		[styles.primary]: appearance === 'primary',
 		[styles.ghost]: appearance === 'ghost'
 	})}
+		{...props}
 	>
 		{children}
-	</button>;
+	</button >;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 export default function Home(): JSX.Element {
 
   return (
     <div>
       <Htag tag='h2'>Hello</Htag>
+      <Button appearance='primary'>Helo</Button>
+      <Button appearance='ghost'>ho</Button>
     </div>
   );
 }
