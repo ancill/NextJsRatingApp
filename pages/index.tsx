d82e1d2bab1b4a7a6f8ad8f5e3c91dbb4478852d
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2022-04-21 17:50:10 +0300

    fixed eslint rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import { Paragraph } from '../components/Paragraph/Paragraph';
 
 export default function Home(): JSX.Element {
 
+
+
   return (
     <div>
       <Htag tag='h2'>Hello</Htag>
-      <Button appearance='primary' arrow='right'>Helo</Button>
+      <Button appearance='primary' arrow='right'>Hello</Button>
       <Button appearance='ghost' arrow='down'>ho</Button>
       <Paragraph size='l'>this is paragraph</Paragraph>
       <Tag color='green' >Hello</Tag>
